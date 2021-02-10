a9ad4a1f7b03f511f3070740b9e954857a1e3d13
Olga Thompson <olgatj2007@gmail.com>
2021-02-10 17:39:24 -0500

    menu models setup

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()  # noqa
 from app import app, db
-from app.models import Employee
-
+from app.models import Employee, Menu, MenuItem, MenuItemType
 
 # Regardless of the lint error you receive,
 # load_dotenv must run before running this
@@ -17,4 +16,23 @@ with app.app_context():
     employee = Employee(name="Margot", employee_number=1234,
                         password="password")
     db.session.add(employee)
+
+    beverages = MenuItemType(name="Beverages")
+    entrees = MenuItemType(name="Entrees")
+    sides = MenuItemType(name="Sides")
+
+    dinner = Menu(name="Dinner")
+
+    fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
+    drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
+    jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+
+    db.session.add(beverages)
+    db.session.add(entrees)
+    db.session.add(sides)
+    db.session.add(dinner)
+    db.session.add(fries)
+    db.session.add(drp)
+    db.session.add(jambalaya)
+
     db.session.commit()
